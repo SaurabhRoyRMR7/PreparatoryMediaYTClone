9a87c2086007f0904a5e37e52973e98d0cd23c2e
Saurabh Roy <saurabhroyrmresm@gmail.com>
2023-06-09 13:28:22 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+import SportsCricketIcon from '@mui/icons-material/SportsCricket';
+import SportsVolleyballIcon from '@mui/icons-material/SportsVolleyball';
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+
+export const categories = [
+  { name: 'India News', icon: <HomeIcon />, },
+  { name: 'DSA Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'MongoDB', icon: <CodeIcon />, },
+  { name: 'NodeJS', icon: <CodeIcon />, },
+  { name: 'ExpressJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'IIT-JEE', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Cricket', icon: <SportsCricketIcon />, },
+  { name: 'Volleyball', icon: <SportsVolleyballIcon/>, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
